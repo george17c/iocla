610838d7e1c5d4b86be1bea4e223d798c8ecf47c
Carpinisan George <gcarpinisan.04@gmail.com>
2024-04-18 18:39:26 +0300

    Solved ex3,4
    I updated the struct fields and printed the values like in ex2
    
    Signed-off-by: Carpinisan George <gcarpinisan.04@gmail.com>

--- laborator/content/structuri-vectori-siruri/3-4-getters-setters/getter-setter-printf.asm
+++ laborator/content/structuri-vectori-siruri/3-4-getters-setters/getter-setter-printf.asm
@@ -18,6 +18,10 @@ section .data
     new_char db 'b'
     new_string db 'Are you sure?', 0
 
+format_x db "int_x: %d", 10, 0
+format_y db "char_y: %c", 10, 0
+format_s db "string_s: %s", 10, 0
+
 section .text
 extern printf
 global main
@@ -31,18 +35,72 @@ main:
     ; Hint: use "lea reg, [base + offset]" to save the result of
     ; "base + offset" into register "reg"
 
+    mov eax, dword [sample_obj + int_x]
+    push eax
+    push format_x
+    call printf
+    add esp, 8
+
+    movzx eax, byte [sample_obj + char_y]
+    push eax
+    push format_y
+    call printf
+    add esp, 8
+
+    lea eax, [sample_obj + string_s]
+    push eax
+    push format_s
+    call printf
+    add esp, 8
+
     ; TODO
     ; write the equivalent of "simple_obj->int_x = new_int"
+
+    mov eax, [new_int]
+    mov dword [sample_obj + int_x], eax
 
     ; TODO
     ; write the equivalent of "simple_obj->char_y = new_char"
 
+    mov eax, [new_char]
+    mov byte [sample_obj + char_y], al
+
     ; TODO
     ; write the equivalent of "strcpy(simple_obj->string_s, new_string)"
+
+    mov esi, new_string
+    mov edi, sample_obj + string_s
+    mov ecx, 0
+
+cpy:
+    mov al, [esi + ecx]
+    mov [edi + ecx], al
+    inc ecx
+    cmp byte [esi + ecx], 0
+    jne cpy
+    mov byte [edi + ecx], 0
 
     ; TODO
     ; print all three values again to validate the results of the
     ; three set operations above
+
+    mov eax, dword [sample_obj + int_x]
+    push eax
+    push format_x
+    call printf
+    add esp, 8
+
+    movzx eax, byte [sample_obj + char_y]
+    push eax
+    push format_y
+    call printf
+    add esp, 8
+
+    lea eax, [sample_obj + string_s]
+    push eax
+    push format_s
+    call printf
+    add esp, 8
 
     xor eax, eax
     leave
