5aba278ee7e21697ea531eb5782a27a9a1e20c39
Carpinisan George <gcarpinisan.04@gmail.com>
2024-05-09 18:44:52 +0300

    Solved ex5
    I pushed the params and called reverse_string, then pushed the new string and called puts
    I updated esp after each call
    
    Signed-off-by: Carpinisan George <gcarpinisan.04@gmail.com>

--- laborator/content/apel-functii/5-print-rev-string/print-rev-string.asm
+++ laborator/content/apel-functii/5-print-rev-string/print-rev-string.asm
@@ -2,6 +2,8 @@
 
 section .data
     mystring db "This is my string", 0
+    format db "%s", 0
+    string times 50 db 0
 
 section .text
 
@@ -9,6 +11,28 @@ extern puts
 extern printf
 global main
 
+reverse_string:
+    push ebp
+    mov ebp, esp
+
+    mov eax, dword [ebp+8]
+    mov ecx, dword [ebp+12]
+    add eax, ecx
+    dec eax
+    mov edx, dword [ebp+16]
+
+copy_one_byte:
+    mov bl, byte [eax]
+    mov byte [edx], bl
+    dec eax
+    inc edx
+    loopnz copy_one_byte
+
+    inc edx
+    mov byte [edx], 0
+
+    leave
+    ret
 main:
     push ebp
     mov ebp, esp
@@ -23,8 +47,19 @@ test_one_byte:
     inc ecx
     jmp test_one_byte
 
+
 out:
+    push string
+    push ecx
+    push mystring
+    call reverse_string
+    add esp, 12
+
     PRINTF32 `[before]: %s\n[after]: \x0`, mystring
+
+    push string
+    call puts
+    add esp, 4
 
     leave
     ret
